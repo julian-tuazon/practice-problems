feb41513c01c2c8277e2d95a6b99c76c2b2f8a3b
Julian Tuazon <jthrow00@gmail.com>
2020-02-09 23:33:35 -0800

    Completed countWords problem from edabit

---
+++ edabit/countWords.js
@@ -0,0 +1,12 @@
+// Create a function that takes a string and returns the word count.The string will be a sentence.
+
+//   Examples
+// countWords("Just an example here move along") ➞ 6
+
+// countWords("This is a test") ➞ 4
+
+// countWords("What an easy task, right") ➞ 5
+
+function countWords(str) {
+  return 1 + str.match(/[" "]/g).length;
+}
