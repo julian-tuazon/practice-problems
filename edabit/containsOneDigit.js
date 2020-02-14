1e081fe70cd84be00dfa153ab7713edbcff32447
Julian Tuazon <jthrow00@gmail.com>
2020-02-14 13:54:34 -0800

    Completed containsOneDigit problem from edabit

---
+++ edabit/containsOneDigit.js
@@ -0,0 +1,14 @@
+// Write a regular expression that matches a string if it contains at least one digit.
+
+//   Examples
+// "c8" ➞ true
+
+// "23cc4" ➞ true
+
+// "abwekz" ➞ false
+
+// "sdfkxi" ➞ false
+// Notes
+// This challenge is designed to use RegEx only.
+
+let x = /\d/;
