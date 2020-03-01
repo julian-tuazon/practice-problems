dd13b47aa4f6c104d62609bde24448705821b63d
Julian <jthrow00@gmail.com>
2020-03-01 00:39:02 -0800

    Completed convert number to dashes problem from edabit

---
+++ edabit/numToDash.js
@@ -0,0 +1,14 @@
+// Create a function that takes a number(from 1 - 60) and returns a corresponding string of hyphens.
+
+//   Examples
+// Go(1) ➞ "-"
+
+// Go(5) ➞ "-----"
+
+// Go(3) ➞ "---"
+// Notes
+// You will be provided integers ranging from 1 to 60.
+
+function Go(num) {
+  return '-'.repeat(num);
+}
