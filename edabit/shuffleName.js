639dda131b25333e7954aa371552daa04ba3493e
Julian <jthrow00@gmail.com>
2020-03-07 12:48:44 -0800

    Solved shuffle name problem from edabit

---
+++ edabit/shuffleName.js
@@ -0,0 +1,12 @@
+// Create a function that accepts a string(of a persons first and last name) and returns a string with the first and last name swapped.
+
+//   Examples
+// nameShuffle("Donald Trump") ➞ "Trump Donald"
+
+// nameShuffle("Rosie O'Donnell") ➞ "O'Donnell Rosie"
+
+// nameShuffle("Seymour Butts") ➞ "Butts Seymour"
+
+function nameShuffle(str) {
+  return str.split(' ').reverse().join(' ');
+}
