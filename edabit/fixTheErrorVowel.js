79a5b82236456b093cb56f7a3ffdabb5ae0a45f8
Julian <jthrow00@gmail.com>
2020-03-12 17:55:12 -0700

    Solved fix the error: vowel edition problem from edabit

---
+++ edabit/fixTheErrorVowel.js
@@ -0,0 +1,14 @@
+// Your friend is trying to write a function that removes all vowels from a string.
+// Fix your friend's code so that it actually does remove all vowels.
+
+//   Examples
+// removeVowels("candy") ➞ "cndy"
+
+// removeVowels("hello") ➞ "hllo"
+// // The "e" is removed, but the "o" is still there!
+
+// removeVowels("apple") ➞ "pple"
+
+function removeVowels(str) {
+  return str.replace(/[aeiou]/g, '')
+}
