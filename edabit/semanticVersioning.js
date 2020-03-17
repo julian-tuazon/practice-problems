1f3964611581010f3ad955ce4852a61d930c667e
Julian <jthrow00@gmail.com>
2020-03-16 22:27:25 -0700

    Solved Semantic Versioning problem from Edabit

---
+++ edabit/semanticVersioning.js
@@ -0,0 +1,34 @@
+// Semantic Versioning
+// In semantic versioning a piece of software can be represented in a format like this example: 6.1.9.
+
+// The first number is the major version.
+// The second number is the minor version.
+// The third number is the patch(bug fixes).
+// Write three separate functions, one to retrieve each element in the semantic versioning specification.
+
+//   Examples
+// // 6.1.9
+// retrieveMajor("6.1.9") ➞ "6"
+
+// retrieveMinor("6.1.9") ➞ "1"
+
+// retrievePatch("6.1.9") ➞ "9"
+
+// // 2.1.0
+// retrieveMajor("2.1.0") ➞ "2"
+
+// retrieveMinor("2.1.0") ➞ "1"
+
+// retrievePatch("2.1.0") ➞ "0"
+
+function retrieveMajor(semver) {
+  return semver.slice(0, semver.indexOf('.'));
+}
+
+function retrieveMinor(semver) {
+  return semver.slice(semver.indexOf('.') + 1, semver.indexOf('.', semver.indexOf('.') + 1));
+}
+
+function retrievePatch(semver) {
+  return semver.slice(semver.indexOf('.', semver.indexOf('.') + 1) + 1);
+}
