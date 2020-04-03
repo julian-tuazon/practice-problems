c3e0eafa76aafcfd7f9074493a4e3f5ac8396174
Julian <jthrow00@gmail.com>
2020-04-03 01:20:05 -0700

    Solved Reverse the Odd Length Words problem from Edabit

---
+++ edabit/reverseOddLengthWords.js
@@ -0,0 +1,21 @@
+// Reverse the Odd Length Words
+// Given a string, reverse all the words which have odd length.The even length words are not changed.
+
+//   Examples
+// reverseOdd("Bananas") ➞ "sananaB"
+
+// reverseOdd("One two three four") ➞ "enO owt eerht four"
+
+// reverseOdd("Make sure uoy only esrever sdrow of ddo length")
+// ➞ "Make sure you only reverse words of odd length"
+// Notes
+// There is exactly one space between each word and no punctuation is used.
+
+function reverseOdd(str) {
+  return str.split(' ').map(x => {
+    if (x.length % 2 !== 0) {
+      return x.split('').reverse().join('')
+    }
+    return x;
+  }).join(' ');
+}
