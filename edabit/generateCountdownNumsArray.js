187ed7faead922123b86e4a9b4ee13ddc9bc31a7
Julian <jthrow00@gmail.com>
2020-04-04 19:50:10 -0700

    Solved Generate a Countdown of Numbers in an Array problem from Edabit

---
+++ edabit/generateCountdownNumsArray.js
@@ -0,0 +1,16 @@
+// Generate a Countdown of Numbers in an Array
+// Create a function that takes a number as an argument and returns an array of numbers counting down from this number to zero.
+
+//   Examples
+// countdown(5) ➞[5, 4, 3, 2, 1, 0]
+
+// countdown(1) ➞[1, 0]
+
+// countdown(0) ➞[0]
+// Notes
+// The argument will always be greater than or equal to zero.
+
+function countdown(start) {
+  if (start === 0) return [start];
+  return [start].concat([countdown(--start)].flat());
+}
