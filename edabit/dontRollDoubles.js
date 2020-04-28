2c818baf8da096a1ad3c47e5a469c7ec8a62f92c
Julian <jthrow00@gmail.com>
2020-04-28 15:13:13 -0700

    Solved Don't Roll Doubles! problem from Edabit

---
+++ edabit/dontRollDoubles.js
@@ -0,0 +1,29 @@
+// Don't Roll Doubles!
+// John is playing a dice game.The rules are as follows.
+
+// Roll two dice.
+// Add the numbers on the dice together.
+// Add the total to your overall score.
+// Repeat this for three rounds.
+// But if you roll DOUBLES, your score is instantly wiped to 0 and your game ends immediately!
+
+// Create a function which takes in a matrix as input, and return John's score after his game has ended.
+
+// Examples
+// diceGame([[1, 2], [3, 4], [5, 6]]) ➞ 21
+
+// diceGame([[1, 1], [5, 6], [6, 4]]) ➞ 0
+
+// diceGame([[4, 5], [4, 5], [4, 5]]) ➞ 27
+// Notes
+// Ignore all other arrays in the matrix if a throw happens to be doubles and go straight to returning 0.
+// John only has two dice and will always give you outcomes for three rounds.
+
+function diceGame(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === arr[i][1]) return 0;
+    sum += (arr[i][0] + arr[i][1]);
+  }
+  return sum;
+}
