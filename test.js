41539832d8f6605830e12c50a9b2152110cd8cd9
Julian <jthrow00@gmail.com>
2020-05-24 23:39:00 -0700

    Created main test js to run specific test suites

---
+++ test.js
@@ -0,0 +1,6 @@
+const { join, resolve } = require('path');
+const { expect } = require('chai');
+
+describe('practice problem tests', () => {
+  require(join(__dirname, 'tests/rockPaperScissors.test'));
+});
