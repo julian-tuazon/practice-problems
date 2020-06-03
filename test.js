ab0dcbc8ac27099c190f985a142a54876d3269b8
Julian <jthrow00@gmail.com>
2020-06-02 23:40:10 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/thePerrinSequence.test'));
+  require(resolve(__dirname, 'tests/edabit/hittingTheJackpot.test'));
 });
