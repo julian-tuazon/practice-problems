ecc668f62fd16fc020748f4da4a423f27c06b070
Julian <jthrow00@gmail.com>
2020-06-03 21:14:53 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/hittingTheJackpot.test'));
+  require(resolve(__dirname, 'tests/edabit/enharmonicEquivalents.test'));
 });
