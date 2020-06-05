f672d716124d3b223b4a6c40028a3f67aa0a1899
Julian <jthrow00@gmail.com>
2020-06-04 19:30:35 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/enharmonicEquivalents.test'));
+  require(resolve(__dirname, 'tests/edabit/multiplyingNumsInString.test'));
 });
