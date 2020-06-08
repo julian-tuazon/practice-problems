d6a1d394d675419203cb76840e78d24feb062a73
Julian <jthrow00@gmail.com>
2020-06-07 23:21:03 -0700

    Solved One Odd and One Even problem from Edabit

---
+++ edabit/oneOddOneEven.js
@@ -0,0 +1,19 @@
+// One Odd and One Even
+// Given a two digit number, return true if that number contains one even and one odd digit.
+
+//   Examples
+// oneOddOneEven(12) ➞ true
+
+// oneOddOneEven(55) ➞ false
+
+// oneOddOneEven(22) ➞ false
+
+function oneOddOneEven(n) {
+  const test = { odd: false, even: false };
+  const nums = n.toString();
+  for (const num of nums) {
+    if (Number(num) % 2 === 0) test.even = true;
+    else test.odd = true;
+  }
+  return test.odd && test.even;
+}
