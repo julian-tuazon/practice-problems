25753416690772911e5601aff27ca0c236248c93
Julian <jthrow00@gmail.com>
2020-06-07 23:25:20 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/howMuchIsTrue.test'));
+  require(resolve(__dirname, 'tests/edabit/oneOddOneEven.test'));
 });
