aca48c764105d00e25fdb56b166bcf43ad62d517
Julian <jthrow00@gmail.com>
2020-06-08 22:31:43 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/oneOddOneEven.test'));
+  require(resolve(__dirname, 'tests/edabit/leapYears.test'));
 });
