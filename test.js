ba3b8c371a0cf4f97f820f5c4c877e9d7e6ffb26
Julian <jthrow00@gmail.com>
2020-06-10 23:11:26 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/barbecueSkewers.test'));
+  require(resolve(__dirname, 'tests/edabit/isJohnnyMakingProgress.test'));
 });
