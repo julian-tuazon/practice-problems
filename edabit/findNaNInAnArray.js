2edd774af17e6d264d3327afc6f04a9c23d11cb3
Julian <jthrow00@gmail.com>
2020-06-12 22:38:34 -0700

    Solved Find NaN in an Array problem from Edabit

---
+++ edabit/findNaNInAnArray.js
@@ -0,0 +1,18 @@
+// Find NaN in an Array
+// Create a function to find NaN in an array of numbers.The return value should be the index where NaN is found.If NaN is not found in the array, then return -1.
+
+// Examples
+// findNaN([1, 2, NaN]) ➞ 2
+
+// findNaN([NaN, 1, 2, 3, 4]) ➞ 0
+
+// findNaN([0, 1, 2, 3, 4]) ➞ -1
+// Notes
+// Inputs are array of numbers.
+
+function findNaN(number) {
+  for (let i = 0; i < number.length; i++) {
+    if (Number.isNaN(number[i])) return i;
+  }
+  return -1;
+}
