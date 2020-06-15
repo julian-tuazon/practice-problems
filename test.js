8d0ea3099678b450c07b17b851f1be14a98e0f55
Julian <jthrow00@gmail.com>
2020-06-14 19:46:08 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/purgeAndOrganize.test'));
+  require(resolve(__dirname, 'tests/edabit/findSecondOccurrenceOfZip.test'));
 });
