0f50ba0004c99caad2c7b1232785c99ff97281b2
Julian <jthrow00@gmail.com>
2020-06-18 19:30:24 -0700

    Added extra cases for unit tests for matchLastItem

--- tests/edabit/matchTheLastItem.test.js
+++ tests/edabit/matchTheLastItem.test.js
@@ -5,6 +5,15 @@ describe('matchLastItem(arr)', () => {
   context('arr items match the last item', () => {
     it('should return true', () => {
       expect(matchLastItem(['my', 'smol', 'cat', 'mysmolcat'])).to.equal(true);
+      expect(matchLastItem(['the', 1, 'icecream', 'the1icecream'])).to.equal(true);
+      expect(matchLastItem([1, true, 'cat', '1truecat'])).to.equal(true);
+    });
+  });
+  context('arr items do not match the last item', () => {
+    it('should return false', () => {
+      expect(matchLastItem(['my', 'large', 'cat', 'mysmolcat'])).to.equal(false);
+      expect(matchLastItem([1, 'large', 'cat', 'mysmolcat'])).to.equal(false);
+      expect(matchLastItem([1, true, 'cat', 'mysmolcat'])).to.equal(false);
     });
   });
 });
