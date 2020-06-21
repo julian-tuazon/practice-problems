bdeb92ca210490a8f28bcd9b0ec573ce161cb6a4
Julian <jthrow00@gmail.com>
2020-06-20 22:25:16 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/volumeOfACone.test'));
+  require(resolve(__dirname, 'tests/edabit/reverseTheNum.test'));
 });
