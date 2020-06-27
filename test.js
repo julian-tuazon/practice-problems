a4b9d05ea4accfd43413c12ad38c438a3ede8eb0
Julian <jthrow00@gmail.com>
2020-06-26 18:30:35 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/oddUpEvenDownNTimes.test'));
+  require(resolve(__dirname, 'tests/edabit/transformUpvotes.test'));
 });
