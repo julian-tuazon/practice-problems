6c687a983f120621310b0d27b3cdab8dd03d394b
Julian <jthrow00@gmail.com>
2020-06-27 18:27:13 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/transformUpvotes.test'));
+  require(resolve(__dirname, 'tests/edabit/negativeImage.test'));
 });
