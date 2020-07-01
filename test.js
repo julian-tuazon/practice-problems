82569e0a3d520434d8cf4d83cb243a54230cc2c0
Julian <jthrow00@gmail.com>
2020-06-30 22:42:48 -0700

    Modified main test file

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const { resolve } = require('path');
 
 describe('practice problem tests', () => {
-  require(resolve(__dirname, 'tests/edabit/solvingExpEquWithLog.test'));
+  require(resolve(__dirname, 'tests/edabit/removeSpecCharFromStr.test'));
 });
