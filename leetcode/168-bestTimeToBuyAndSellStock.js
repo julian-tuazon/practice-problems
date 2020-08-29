f52c67f139511c6c22dc6ad506bb00d35e648152
Julian <jthrow00@gmail.com>
2020-08-29 05:07:55 -0700

    Solved Problem 168 - Best Time to Buy and Sell Stock from LeetCode

---
+++ leetcode/168-bestTimeToBuyAndSellStock.js
@@ -0,0 +1,35 @@
+// 121. Best Time to Buy and Sell Stock
+
+// Say you have an array for which the ith element is the price of a given stock on day i.
+
+// If you were only permitted to complete at most one transaction(i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+// Note that you cannot sell a stock before you buy one.
+
+//   Example 1:
+
+// Input: [7, 1, 5, 3, 6, 4]
+// Output: 5
+// Explanation: Buy on day 2(price = 1) and sell on day 5(price = 6), profit = 6 - 1 = 5.
+// Not 7 - 1 = 6, as selling price needs to be larger than buying price.
+//   Example 2:
+
+// Input: [7, 6, 4, 3, 1]
+// Output: 0
+// Explanation: In this case, no transaction is done, i.e.max profit = 0.
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  if (prices.length < 2) return 0;
+  let lowest = prices[0];
+  let highestDifference = prices[1] - prices[0];
+  for (let i = 2; i < prices.length; i++) {
+    lowest = Math.min(prices[i - 1], lowest);
+    const currentDifference = prices[i] - lowest;
+    if (currentDifference > highestDifference) highestDifference = currentDifference;
+  }
+  return highestDifference > 0 ? highestDifference : 0;
+};
