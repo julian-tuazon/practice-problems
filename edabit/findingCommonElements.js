f6e42bc91e60b8d3af04fc56a024b6aa5cd9dfe9
Julian <jthrow00@gmail.com>
2020-09-07 22:34:00 -0700

    Solved Finding Common Elements problem from Edabit

---
+++ edabit/findingCommonElements.js
@@ -0,0 +1,23 @@
+// Finding Common Elements
+// Create a function that takes two "sorted" arrays of numbers and returns an array of numbers which are common to both the input arrays.
+
+//   Examples
+// commonElements([-1, 3, 4, 6, 7, 9], [1, 3]) ➞[3]
+
+// commonElements([1, 3, 4, 6, 7, 9], [1, 2, 3, 4, 7, 10]) ➞[1, 3, 4, 7]
+
+// commonElements([1, 2, 2, 2, 3, 4, 5], [1, 2, 4, 5]) ➞[1, 2, 4, 5]
+
+// commonElements([1, 2, 3, 4, 5], [10, 12, 13, 15]) ➞[]
+
+function commonElements(arr1, arr2) {
+  const cache = {};
+  const commonNums = [];
+  for (const num of arr1) {
+    cache[num] = true;
+  }
+  for (const num of arr2) {
+    if (cache[num]) commonNums.push(num);
+  }
+  return commonNums;
+}
