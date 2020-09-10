f6b3c200a03cd452f5db7a24f27f31b426ad3ba4
Julian <jthrow00@gmail.com>
2020-09-10 12:07:38 -0700

    Solved Factor Tractor problem from Edabit

---
+++ edabit/factorTractor.js
@@ -0,0 +1,26 @@
+// Factor Tractor
+// Write a program to find all the prime factors of a given number.The program must return an array containing all the prime factors, sorted in ascending order.Remember that 1 is neither prime nor composite and should not be included in your output array.
+
+//   Examples
+// primeFactorize(25) ➞[5, 5]
+
+// primeFactorize(19) ➞[19]
+
+// primeFactorize(77) ➞[7, 11]
+// Notes
+// Output array must be sorted in ascending order
+// The only positive integer which is neither prime nor composite is 1. Return an empty array if 1 is the input.
+
+function primeFactorize(num) {
+  if (num < 2) return [];
+  return (function helper(currentNum, factors) {
+    if (num === 1) return factors;
+    if (num % currentNum === 0) {
+      factors.push(currentNum);
+      num /= currentNum;
+      return helper(2, factors);
+    } else {
+      return helper(++currentNum, factors);
+    }
+  })(2, []);
+}
