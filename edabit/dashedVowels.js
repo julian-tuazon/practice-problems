535fb47bd65b902329817aef052faf95317b6efc
Julian <jthrow00@gmail.com>
2020-10-17 23:20:28 -0700

    Solved Dashed Vowels problem from Edabit

---
+++ edabit/dashedVowels.js
@@ -0,0 +1,21 @@
+// Dashed Vowels
+// Create a function that takes a string and returns dashes on the left and right side of every vowel(a e i o u).
+
+//   Examples
+// dashed("Edabit") ➞ "-E-d-a-b-i-t"
+
+// dashed("Carpe Diem") ➞ "C-a-rp-e- D-i--e-m"
+
+// dashed("Fight for your right to party!") ➞ "F-i-ght f-o-r y-o--u-r r-i-ght t-o- p-a-rty!"
+// Notes
+// A string can contain uppercase and lowercase vowels.
+// Y is not considered a vowel.
+
+function dashed(str) {
+  let newStr = '';
+  for (const char of str) {
+    if (/[aeiouAEIOU]/.test(char)) newStr += `-${char}-`;
+    else newStr += char;
+  }
+  return newStr;
+}
